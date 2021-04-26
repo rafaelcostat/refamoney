0b67e4eb703e667006c7cea308e71e9f228f843f
Rafael Costa <rafaelcostat@gmail.com>
2021-04-26 16:44:25 -0300

    global style configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
+
 export function App() {
   return (
     <div className="App">
       <h1>Refa Money</h1>
+      <GlobalStyle />
     </div>
   );
 }
